634b70c2b762f90fda65385dd0bbfef146d0f227
Joice Cristina <71676439+joicecriss@users.noreply.github.com>
2024-03-21 17:23:44 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 // Componentes
-import { LoginComponents } from './login/login.component'
+import { LoginComponent } from './login/login.component'
 
 const routes: Routes = [
   {
